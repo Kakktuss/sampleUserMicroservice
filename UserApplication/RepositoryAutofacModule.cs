4dea6b988cdf5acd13424ddf9367bdc8f4c2ac07
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-24 19:34:56 +0200

    [Add] Application: Repository and service autofac module

---
+++ UserApplication/RepositoryAutofacModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using UserApplication.EntityFrameworkDataAccess.Repositories;
+
+namespace UserApplication
+{
+    public class RepositoryAutofacModule : Autofac.Module
+    {
+        private readonly string _connectionString;
+
+        public RepositoryAutofacModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // EF Core Configuration
+            builder.RegisterType<CountryRepository>()
+                .As<ICountryRepository>()
+                .AsSelf();
+            
+            builder.RegisterType<UserRepository>()
+                .As<IUserRepository>()
+                .AsSelf();
+            
+            // Dapper Configuration
+            builder.Register(c => new DapperDataAccess.Repositories.UserRepository(_connectionString))
+                .As<DapperDataAccess.Repositories.IUserRepository>()
+                .AsSelf();
+        }
+    }
+}
---
+++ UserApplication/ServiceAutofacModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using UserApplication.Services;
+
+namespace UserApplication
+{
+    public class ServiceAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserService>()
+                .As<IUserService>()
+                .AsSelf();
+        }
+    }
+}
