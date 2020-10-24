5b7319a6ffe45181d12716046c9bb1c380504a0e
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-24 19:16:16 +0200

    [Add] Application: View models to link controllers with services

---
+++ UserApplication/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace UserApplication.ViewModels
+{
+    public class UserViewModel
+    {
+        
+        public string Uuid { get; set; }
+        
+        public string Username { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string IsCompleted { get; set; }
+        
+    }
+}
