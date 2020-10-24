8fc3907d0f400728e376f27d7a62df5c0990033e
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-24 19:39:53 +0200

    [Add] Api: Complementary authorization modules to use permissions in JWT

---
+++ UserApi/Authorization/PermissionsAuthorizationRequirement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UserApi.Authorization
+{
+    public class PermissionsAuthorizationRequirement : AuthorizationHandler<PermissionsAuthorizationRequirement>, IAuthorizationRequirement
+    {
+        public IEnumerable<string> RequiredPermissions { get; }
+
+        public PermissionsAuthorizationRequirement(IEnumerable<string> requiredPermissions)
+        {
+            if (requiredPermissions == null || !requiredPermissions.Any())
+            {
+                throw new ArgumentException($"{nameof(requiredPermissions)} must contain at least one value.", nameof(requiredPermissions));
+            }
+ 
+            RequiredPermissions = requiredPermissions;
+        }
+        
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionsAuthorizationRequirement requirement)
+        {
+            if (context.User != null)
+            {
+                var permissionsClaim = context.User.Claims.Where(c =>
+                    string.Equals(c.Type, "permissions", StringComparison.OrdinalIgnoreCase));
+
+                if (permissionsClaim.Any())
+                {
+                    var permissions = permissionsClaim.Select(e => e.Value);
+
+                    if (requirement.RequiredPermissions.All(requiredPermission => permissions.Contains(requiredPermission)))
+                    {
+                        context.Succeed(requirement);
+                    }
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UserApi/Authorization/PermissionsAuthorizationRequirementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UserApi.Authorization
+{
+    public static class ScopeAuthorizationRequirementExtensions
+    {
+        public static AuthorizationPolicyBuilder RequirePermission(
+            this AuthorizationPolicyBuilder authorizationPolicyBuilder,
+            params string[] requiredScopes)
+        {
+            authorizationPolicyBuilder.RequirePermission((IEnumerable<string>) requiredScopes);
+            return authorizationPolicyBuilder;
+        }
+ 
+        public static AuthorizationPolicyBuilder RequirePermission(
+            this AuthorizationPolicyBuilder authorizationPolicyBuilder,
+            IEnumerable<string> requiredScopes)
+        {
+            authorizationPolicyBuilder.AddRequirements(new PermissionsAuthorizationRequirement(requiredScopes));
+            return authorizationPolicyBuilder;
+        }
+    }
+}
