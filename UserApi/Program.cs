561a37751f34c902d2da5cc6726fba1c9e7b6351
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-24 19:45:14 +0200

    [Update] Api: Entrypoints of the application; added authorization rules, cors and many other features

--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -1,6 +1,10 @@
+using System;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Loki;
 
 namespace UserApi
 {
@@ -8,14 +12,49 @@ namespace UserApi
     {
         public static void Main(string[] args)
         {
+            ConfigureLogging();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(15);
+                    });
+                })
+                .UseSerilog()
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory());
+        }
+        
+        public static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var lokiCredentials = new NoAuthCredentials(configuration.GetSection("Logging").GetSection("Loki")["Uri"]);
+            
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .MinimumLevel.Verbose()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.LokiHttp(lokiCredentials)
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
     }
 }
--- UserApi/Startup.cs
+++ UserApi/Startup.cs
@@ -4,11 +4,10 @@ using System.Reflection;
 using System.Text;
 using Autofac;
 using BuildingBlock.Bus.Abstractions.Nats.Events;
-using BuildingBlock.Bus.Nats;
+using BuildingBlock.Bus.Stan;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -17,8 +16,17 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Prometheus;
+using Prometheus.DotNetRuntime;
+using Prometheus.SystemMetrics;
+using Prometheus.SystemMetrics.Collectors;
+using Serilog;
+using STAN.Client;
+using UserApi.Authorization;
 using UserApplication;
-using UserInfrastructure.EntityFrameworkDataAccess;
+using UserApplication.EntityFrameworkDataAccess;
+using UserApplication.IntegrationEvents.Events.Country;
+using UserApplication.IntegrationEvents.Handlers.Country;
 
 namespace UserApi
 {
@@ -35,9 +43,10 @@ namespace UserApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Database services
             services.AddEntityFrameworkSqlServer();
 
-            services.AddDbContext<UserContext>((serviceProvider, optionsBuilder) =>
+            services.AddDbContextPool<UserContext>((serviceProvider, optionsBuilder) =>
             {
                 optionsBuilder
                     .UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"));
@@ -45,6 +54,7 @@ namespace UserApi
                 optionsBuilder.UseInternalServiceProvider(serviceProvider);
             });
 
+            // Swagger service
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "User api", Version = "v1"});
@@ -54,6 +64,7 @@ namespace UserApi
                 c.IncludeXmlComments(xmlPath);
             });
 
+            // Authorization services
             IdentityModelEventSource.ShowPII = true;
 
             services.AddAuthentication(options =>
@@ -63,69 +74,191 @@ namespace UserApi
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(options =>
             {
-                options.Authority = "https://dev-q74vz31h.eu.auth0.com/";
-                options.Audience = "http://api.seo-reborn.com/";
+                options.Authority = Configuration.GetSection("ThirdParty").GetSection("Auth0")["TenantName"];
+                options.Audience = Configuration.GetSection("ThirdParty").GetSection("Auth0")["Audience"];
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes("UCNyMRctIR3CIqqwKodTNqSTRWuyfSfYL0L-7fQ-46P_dZpQi-oT_ghQdV-sqE3X"))
+                    RoleClaimType = "https://seo-reborn.com/roles"
                 };
                 options.RequireHttpsMetadata = false;
             });
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("user:self_read", builder => builder.RequirePermission("user:self_read"));
+                
+                options.AddPolicy("user:self_delete", builder => builder.RequirePermission("user:self_delete"));
+                
+                options.AddPolicy("user:self_update", builder => builder.RequirePermission("user:self_update"));
+            });
+
+            // Cors services
+            services.AddCors(options =>
+            {
+                options.AddPolicy("developerPolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .AllowAnyHeader();
+                });
+                
+                options.AddPolicy("prodPolicy", builder =>
+                {
+                    builder.WithOrigins("https://seo-reborn.com")
+                        .WithMethods("GET", "DELETE", "POST")
+                        .AllowCredentials()
+                        .AllowAnyHeader();
+                });
+                
+                options.AddPolicy("preProdPolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .AllowAnyHeader();
+                    
+                    builder.WithOrigins("https://seo-reborn.com")
+                        .WithMethods("GET", "DELETE", "POST")
+                        .AllowCredentials()
+                        .AllowAnyHeader();
+                });
+            });
+            
+            // Api versioning services
             services.AddApiVersioning(options =>
             {
                 options.DefaultApiVersion = new ApiVersion(1, 1);
 
                 options.ReportApiVersions = true;
             });
+            
+            // Health checks services
+            services.AddHealthChecks();
+
+            // Metrics services
+            services.AddSystemMetrics(false);
+            services.AddSystemMetricCollector<CpuUsageCollector>();
+            services.AddSystemMetricCollector<MemoryCollector>();
+            services.AddSystemMetricCollector<NetworkCollector>();
+            services.AddSystemMetricCollector<LoadAverageCollector>();
+            
+            // Caching services
+            services.AddResponseCaching();
 
             services.AddControllers();
+        }
+        
+        public virtual void ConfigureMetrics(IApplicationBuilder app)
+        {
+            var callsCounter = Metrics.CreateCounter("request_total", "Counts the requests to the Country API endpoints", new CounterConfiguration()
+            {
+                LabelNames = new[] { "method", "endpoint" }
+            });
+
+            app.Use((context, next) =>
+            {
+                callsCounter.WithLabels(context.Request.Method, context.Request.Path);
+
+                return next();
+            });
+            
+            IDisposable collector = DotNetRuntimeStatsBuilder
+                .Customize()
+                .WithContentionStats()
+                .WithJitStats()
+                .WithThreadPoolSchedulingStats()
+                .WithThreadPoolStats()
+                .WithGcStats()
+                .WithExceptionStats()
+                .StartCollecting();
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
-            builder.RegisterModule(new MediatorAutofacModule());
+            builder.RegisterModule(new RepositoryAutofacModule(Configuration.GetConnectionString("DefaultConnectionString")));
 
-            // builder.RegisterModule(new RepositoryAutofacModule());
-
+            builder.RegisterModule(new ServiceAutofacModule());
+            
             builder.RegisterModule(new IntegrationEventBusAutofacModule());
 
-            builder.RegisterModule(new NatsBusesAutofacModule(Configuration
+            var options = StanOptions.GetDefaultOptions();
+            options.NatsURL = Configuration
                 .GetSection("Bus")
-                .GetSection("Nats")["ConnectionString"], Configuration
+                .GetSection("Nats")["Url"];
+
+            var appName = Configuration
                 .GetSection("Bus")
-                .GetSection("Nats").GetSection("IsSecureConnection").Get<bool>()));
+                .GetSection("Nats")["AppName"];
+
+            appName = $"{appName}_{Guid.NewGuid()}";
+            
+            builder.RegisterModule(new StanBusesAutofacModule(Configuration
+                    .GetSection("Bus")
+                    .GetSection("Nats")["ClusterName"], 
+                appName, 
+                options));
 
             builder.RegisterBuildCallback(container =>
             {
-                var eventBus = container.Resolve<INatsIntegrationEventBus>();
+                var eventBus = container.Resolve<IStanIntegrationEventBus>();
+                
+                var options = StanSubscriptionOptions.GetDefaultOptions();
+                options.StartWithLastReceived();
+                options.ManualAcks = true;
+                options.AckWait = 10000;
+                options.DurableName = "user_microservice";
 
-                /*
-                 *  eventBus.Subscribe<StoreLicenseAddedIntegrationEvent, 
-                     StoreLicenseAddedIntegrationEventHandler>("store.shopify.license", "created");
-                 */
+                eventBus.Subscribe<CountryCreatedIntegrationEvent, CountryCreatedIntegrationEventHandler>(
+                    "country", "created", "user_microservice", options);
+                
+                eventBus.Subscribe<CountryDeletedIntegrationEvent, CountryDeletedIntegrationEventHandler>(
+                    "country", "deleted", "user_microservice", options);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                
+                app.UseSwagger();
 
-            app.UseAuthentication();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User authentication api");
+                });
+            }
 
+            // Configure Routing
             app.UseRouting();
 
+            // Configure API Versioning
+            app.UseApiVersioning();
+            
+            // Configure Authentication and Authorization
+            app.UseAuthentication();
+
             app.UseAuthorization();
+            
+            // Configure Prometheus Metrics
+            app.UseMetricServer();
 
-            app.UseSwagger();
+            app.UseHttpMetrics();
+            
+            ConfigureMetrics(app);
+            
+            // Configure serilog to log requests
+            app.UseSerilogRequestLogging();
 
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "License api"); });
+            // Configure response caching
+            app.UseResponseCaching();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
